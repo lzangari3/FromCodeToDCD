180e9e92dc8b1a5398ffb2f6acaf79747565095e
lzangari3 <133886712+lzangari3@users.noreply.github.com>
2024-07-05 14:42:17 -0400

    Add files via upload
    added method to manager class.

--- Manager.java
+++ Manager.java
@@ -3,4 +3,8 @@ public class Manager extends LeagueEmployee{
     public Manager(String name, double salary, int rating) {
         super(name, salary, rating);
     }
+
+    public void CoachAndGuide(Player player) {
+        System.out.println(this.getName() + " is helping coach " + player.getName());
+    }
 }
